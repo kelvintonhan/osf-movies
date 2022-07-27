1610f1b891699ab7132a76ee16056956247a152f
Kelvin Tonhan <kelvintonhan@gmail.com>
2022-07-27 11:23:09 -0300

    add

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Movie from "./pages/Movie"
 function App() {
   return (
     <div className="App">
-      <BrowserRouter >
+      <BrowserRouter basename={process.env.PUBLIC_URL} >
         <Routes>
           <Route path="/" element={<Home/>} />
           <Route path="/movie/:id" element={<Movie/>} />
